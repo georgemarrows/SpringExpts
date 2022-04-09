34c3ebc719a4547ffbbfbec74ea6d59033bb05df
George Marrows <george.marrows@gmail.com>
2022-04-09 16:58:38 +0200

    Move and rename the Application class

---
+++ src/main/java/georgemarrows/learnspring/CustomerAccountApplication.java
@@ -0,0 +1,18 @@
+package georgemarrows.learnspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAccountApplication {
+
+	// Because the Application sits above controller, repository etc 
+	// it can find all components for auto-wiring. If it sits to one side,
+	// you have to tell SpringBootApplication the packages to scan using 
+	// scanBasePackages
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerAccountApplication.class, args);
+	}
+
+}
